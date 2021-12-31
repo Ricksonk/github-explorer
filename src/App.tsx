8dfc3dc059bedd5dea3a8ffe170523acb8f29ad8
Rodrigo Ferreira <rodrigo@iMac-de-Rodrigo.local>
2021-12-31 14:19:57 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
 
 export function App(){
-    return <RepositoryList />
+    return (
+        <>
+        <Header/>
+        <RepositoryList />
+        <Footer />
+        </>
+    )
+   
+    
 } 
